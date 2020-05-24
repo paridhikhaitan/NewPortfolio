287a5ac6e73d8ea58e0348d3543be67ef17bf437
paridhikhaitan <paridhi4699@gmail.com>
2020-05-24 21:54:28 +0530

    google analytics

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,8 @@ import React from "react";
 
 class Navbar extends React.Component {
   componentDidMount() {
-
-    if(this.props.fromPage==="home"){
-      document.getElementById("home").style.display = "none"
+    if (this.props.fromPage === "home") {
+      document.getElementById("home").style.display = "none";
     }
     if (this.props.fromPage === "work" || this.props.fromPage === "about") {
       document.getElementById(this.props.fromPage).classList.add("active");
@@ -15,25 +14,8 @@ class Navbar extends React.Component {
     return (
       <div className="topNav">
         <div id="home">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="play_arrow_24px">
-              <path
-                id="icon/av/play_arrow_24px"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M8.125 15L21.875 23.75V6.25L8.125 15ZM12.7875 15L19.375 10.8V19.2L12.7875 15Z"
-                fill="white"
-              />
-            </g>
-          </svg>
           <h4>
-            <a href="/">home</a>
+            <a href="/"> {"<  "}home</a>
           </h4>
         </div>
 
@@ -55,7 +37,7 @@ class Navbar extends React.Component {
               href="https://drive.google.com/file/d/1vXAjSAojqlUu8yTRlc57LJhYG0KNH-Xp/view?usp=sharing"
               target="_blank"
             >
-              resumé
+              résumé
             </a>
           </h4>
         </div>
