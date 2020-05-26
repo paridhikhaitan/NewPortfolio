f7e8e8ff91da27cb85f69008e7bbcef32c7498e6
paridhikhaitan <paridhi4699@gmail.com>
2020-05-27 00:08:43 +0530

    removed particle from mobile

--- src/pages/parts.js
+++ src/pages/parts.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Particles from "react-particles-js";
-import { Container, Row, Col } from "react-grid-system";
+import { Hidden, Visible, Container, Row, Col } from "react-grid-system";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 
@@ -8,38 +8,77 @@ class parts extends React.Component {
   render() {
     return (
       <div>
-        <Particles
-          height="100vh"
-          width="100vw"
-          style={{
-            opacity: "0.85",
-            position: "fixed",
-            left: "0",
-            top: "0"
-          }}
-          params={{
-            particles: {
-              number: {
-                value: 70
+        <Hidden xs sm md>
+          <Particles
+            height="100vh"
+            width="100vw"
+            style={{
+              opacity: "0.85",
+              position: "fixed",
+              left: "0",
+              top: "0"
+            }}
+            params={{
+              particles: {
+                number: {
+                  value: 60
+                },
+                move: {
+                  direction: "random",
+                  speed: 1
+                },
+                size: {
+                  value: 3
+                }
               },
-              move: {
-                direction: "random",
-                speed: 1
-              },
-              size: {
-                value: 3
-              }
-            },
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: "repulse"
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "repulse"
+                  }
                 }
               }
-            }
-          }}
-        />
+            }}
+          />
+        </Hidden>
+        <Hidden lg xl>
+          <Particles
+            height="100vh"
+            width="100vw"
+            style={{
+              opacity: "0.85",
+              position: "fixed",
+              left: "0",
+              top: "0",
+            }}
+            params={{
+              particles: {
+                number: {
+                  value: 25
+                },
+                color:{
+                  value: "#a1a1a1"
+                },
+                move: {
+                  direction: "random",
+                  speed: 1
+                },
+                size: {
+                  value: 3,
+                }
+              },
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "repulse"
+                  }
+                }
+              }
+            }}
+          />
+        </Hidden>
         <Navbar fromPage="home" />
         <div className="splashHeading">
           <div>
@@ -47,9 +86,9 @@ class parts extends React.Component {
               Hi, I'm Paridhi!
             </h1>
             <br />
-            <h1 style={{fontFamily: 'Karla', marginTop: "2rem"}}>
-              I'm a web developer + designer who is passionate about building pixel-perfect
-              products and unique experiences for everyone!
+            <h1 style={{ fontFamily: "Karla", marginTop: "2rem" }}>
+              I'm a web developer + designer who is passionate about building
+              pixel-perfect products and unique experiences for everyone!
             </h1>
             <br />
             <h1
@@ -57,8 +96,7 @@ class parts extends React.Component {
                 fontSize: "2rem",
                 lineHeight: "2.8rem",
                 color: "#919191",
-                fontFamily: "Karla",
-               
+                fontFamily: "Karla"
               }}
             >
               {" "}
